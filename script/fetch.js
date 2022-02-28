9388037fedb1dad30afbbf5ac32fe006ee35c376
sinanliu001 <sinan.liu001@umb.edu>
2022-02-28 17:04:04 -0500

    popular

--- script/fetch.js
+++ script/fetch.js
@@ -1,16 +1,4 @@
-// let url = 'https://api.themoviedb.org/3/movie/550?api_key=f4fd559b706454d3e7876ad1c9d54257';
-// fetch(url, option).then(res => res.joson()).then(data => console.log(data))
-// const fetchData = (url, method) => {
-//     let x = new xmlhttprequire();
-//     return new promise(() => {
-//         x.onreadystatechange = function() {}
-//         if (xhr.readystate !== 4) return;
-//         if (xhr.status >= 200 && x.stuats <300) resolve(x);
-//         else {reject('err')}
-//     })
-//     x.open(method,url, ture)
-//     x.send()
-// }
+
 const movie_id_list = [];
 const movie_list = [];
 
@@ -21,15 +9,44 @@ let pre_button = document.querySelector('.pre_button');
 let next_button = document.querySelector('.next_button');
 let button_text = document.querySelector('.text_botton');
 
+let promise_array = [];
+for (var i = 1; i < 500; i++)(
+    promise_array.push(fetch(
+       'https://api.themoviedb.org/3/movie/popular?api_key=f4fd559b706454d3e7876ad1c9d54257&language=en-US&page=1'+i))
+)
+console.log(promise_array.length);
+
 
 fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=f4fd559b706454d3e7876ad1c9d54257&language=en-US')
 .then(res => 
     {return res.json();}
 )
 .then(data => count(data))
-.then(list => {
-    movie(list)})
 .catch(error => console.log('error'));
+
+Promise.all(promise_array)
+    .then(function (responses) {
+        // Get a JSON object from each of the responses
+        return Promise.all(responses.map(function (response) {
+            return response.json();
+        }));
+    })
+    .then(
+        function (data) {
+            // console.log(data)
+            // console.log(movie_id_list)
+             for (var i = 0; i < data.length; i++){
+                if (data[i].results){
+                    data[i].results.forEach(
+                        element => movie_list.push(element)
+                    )
+                }
+            }
+            return movie_list;
+        }
+    )
+    .then(resource => indiv(resource))
+    .catch(function (error) {console.log('moive_error')})
 
 function count(data){
     // console.log(data['genres'].length);
@@ -40,38 +57,12 @@ function count(data){
         // console.log(list_id);
         movie_id_list.push(list_id);
     });
-    // console.log(movie_id_list);
+    console.log(movie_id_list);
     return movie_id_list;
 }
-function movie(list){
-    let promise_array = [];
-    list.forEach(
-        element => promise_array.push(fetch('https://api.themoviedb.org/3/list/'+element+'?api_key=f4fd559b706454d3e7876ad1c9d54257&language=en-US'))
-    )
-    // console.log(promise_array)
-    Promise.all(promise_array)
-    .then(function (responses) {
-        // Get a JSON object from each of the responses
-        return Promise.all(responses.map(function (response) {
-            return response.json();
-        }));
-    })
-    .then(
-        function (data) {
-             for (var i = 0; i < data.length; i++){
-                if (data[i].items){
-                    data[i].items.forEach(
-                        element => movie_list.push(element)
-                    )
-                }
-            }
-            return movie_list;
-        }
-    )
-    .then(resource => indiv(resource))
-    .catch(function (error) {console.log('moive_error')})
-}
+
 function indiv(data){
     //background.style.backgroundImage = "url(../asset/amc.png)"
-    console.log('hello')
+    console.log(data)
+
 }
