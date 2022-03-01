3bb4d24ef1787152cc33fdb9af53c51599b8f633
sinanliu001 <sinan.liu001@umb.edu>
2022-03-01 16:50:21 -0500

    grid_pre

--- script/fetch.js
+++ script/fetch.js
@@ -2,17 +2,18 @@
 const movie_id_list = [];
 const movie_list = [];
 
-let background = document.querySelector('.movie_container');
-background.innerHTML = 'loading';
+const loading_page = document.querySelector('.loading_page');
+const popular_movie_page = document.querySelector('.popular_movie_page');
+const like_list_page = document.querySelector('.like_list_page');
 
-let pre_button = document.querySelector('.pre_button');
-let next_button = document.querySelector('.next_button');
-let button_text = document.querySelector('.text_botton');
+const pre_button = document.querySelector('.pre_button');
+const next_button = document.querySelector('.next_button');
+const button_text = document.querySelector('.text_botton');
 
-let promise_array = [];
-for (var i = 1; i < 500; i++)(
+const promise_array = [];
+for (var i = 1; i < 502; i++)(
     promise_array.push(fetch(
-       'https://api.themoviedb.org/3/movie/popular?api_key=f4fd559b706454d3e7876ad1c9d54257&language=en-US&page=1'+i))
+       'https://api.themoviedb.org/3/movie/popular?api_key=f4fd559b706454d3e7876ad1c9d54257&language=en-US&page='+i))
 )
 console.log(promise_array.length);
 
@@ -64,5 +65,19 @@ function count(data){
 function indiv(data){
     //background.style.backgroundImage = "url(../asset/amc.png)"
     console.log(data)
-
+    loading_page.style.display = "none";
+    popular_movie_page.style.display="flex";
+    
+    // var span = document.createElement("SPAN");
+    // var text = document.createTextNode("w");
+    // span.appendChild(text);
+    // background.appendChild(span);
+    Init(data);
+}
+function Init(data){
+    for (var i = 0; i < 500; i++){
+        let item = document.createElement("DIV");
+        let text_item = document.createTextNode(i);
+        
+    }
 }
